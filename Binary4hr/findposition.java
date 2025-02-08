721d5106703acefd2117c4a0e2f3263108868cfb
TejasSalunkeCode <tejassalunke8788@gmail.com>
2025-02-08 17:24:22 +0530

    upgrade

--- Binary4hr/findposition.java
+++ Binary4hr/findposition.java
@@ -13,6 +13,8 @@ public class findposition {
             end=(end+(end-start+1)*2);
             start=newstart;
         }
+
+        System.out.println("print hello");
          return binarysearch(arr, target, start, end);
     }
 
