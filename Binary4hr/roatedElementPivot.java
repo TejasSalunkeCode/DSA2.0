3cb84fd0383a68246638a6d10f5c4f06d87d5edd
TejasSalunkeCode <tejassalunke8788@gmail.com>
2025-02-10 09:59:26 +0530

    update

--- Binary4hr/roatedElementPivot.java
+++ Binary4hr/roatedElementPivot.java
@@ -2,17 +2,65 @@ public class roatedElementPivot {
     public static void main(String[] args) {
         int []arr={3,4,5,6,7,0,1,2};
         int target=0;
+        System.out.println(findpivot(arr));
     }
-    static int search(int[]arr,int target){
-        int start=0;
-        int end=arr.length-1;
-        int mid=((start+end)/2);
 
-        while ((start<)) {
-            
+    static int search(int[] nums, int target){
+        int pivot=findpivot(nums);
+
+        if(pivot==-1){
+            return binarysearch(nums,target,0,nums.length-1);
         }
 
+        if(nums[pivot]==target){
+            return pivot;
+        }
 
-        return -1;
+        if(target>=nums[0]){
+            return binarysearch(nums, target,0,pivot-1);
+        }
+          return binarysearch(nums, target,pivot+1,nums.length-1);
+
+
+
     }
+
+    static int binarysearch(int []arr,int target,int start, int end){
+        while(start<=end){
+            int mid=start+(end-start)/2;
+
+           if(target<arr[mid]){
+            end=mid-1;
+           }
+           else if(target>arr[mid]){
+               start=mid+1;
+            }
+            else{
+                return mid;
+            }
+    }
+    return -1;
+}
+
+
+    static int findpivot(int[]arr){
+        int start=0;
+        int end=arr.length-1;
+        while ((start<=end)) {
+            int mid=start+(end-start)/2;
+
+            if(mid < end && arr[mid] > arr[mid+1]){
+                return mid;
+            }
+            if(mid > start && arr[mid]<arr[mid-1]){
+                return mid-1;
+            }
+            if(arr[mid]<=arr[start]){
+                end=mid-1;
+            }else{
+                start=mid+1;
+            }
+        }
+        return -1;
+}
 }
