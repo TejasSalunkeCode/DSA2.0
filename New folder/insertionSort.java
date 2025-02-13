c8f5ea2441b03a2609ef291b698fb2393c5e561e
TejasSalunkeCode <tejassalunke8788@gmail.com>
2025-02-13 20:35:03 +0530

    updated

--- New folder/insertionSort.java
+++ New folder/insertionSort.java
@@ -8,7 +8,7 @@ public class insertionSort {
 
     }
     static void insertionn(int []arr){
-        for (int i = 0; i < arr.length-1; i++) {
+        for (int i = 0; i < arr.length-1; i++) { 
             for(int j=i+1;j>0;j--){
                 System.out.println("hello");
                 if(arr[j]<arr[j-1]){
