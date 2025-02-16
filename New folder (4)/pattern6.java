5a1862413fcb212002615cea6738562ad80b83ad
TejasSalunkeCode <tejassalunke8788@gmail.com>
2025-02-16 19:58:59 +0530

    push

--- New folder (4)/pattern6.java
+++ New folder (4)/pattern6.java
@@ -2,7 +2,6 @@ public class pattern6{
     public static void main(String[] args) {
         pattern6(5);
     }
-    
     static void pattern6(int n){
         for (int row = 0; row < 2*n; row++) {
            int totalcolInRow=row>n ? 2*n-row:row;
