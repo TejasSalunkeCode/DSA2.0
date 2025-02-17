1822bbc9355354fa38b3365631c1ce14cc90060d
TejasSalunkeCode <tejassalunke8788@gmail.com>
2025-02-17 20:00:33 +0530

    update

---
+++ New folder (4)/pattern7.java
@@ -0,0 +1,23 @@
+public class pattern7 {
+        public static void main(String[] args) {
+            pattern6(5);
+        }
+        static void pattern6(int n){
+            for (int row = 0; row < 2*n; row++) {
+               int totalcolInRow=row>n ? 2*n-row:row;
+               int noofspaces=n-totalcolInRow;
+               for (int s = 0; s < noofspaces; s++) {
+                System.out.print(" ");
+               }
+               for (int col = 0; col < totalcolInRow; col++) {
+                System.out.print("* ");
+               }  
+               System.out.println();
+            }
+        }
+    }
+    
+    
+    
+    
+
