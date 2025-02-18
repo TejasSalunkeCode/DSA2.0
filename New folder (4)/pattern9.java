988e536e41b4a7d5599c76b3df5f20e0f94b02b2
TejasSalunkeCode <tejassalunke8788@gmail.com>
2025-02-18 20:09:18 +0530

    updated

--- New folder (4)/pattern9.java
+++ New folder (4)/pattern9.java
@@ -1,24 +1,23 @@
 
 public class pattern9 {
-        public static void main(String[] args) {
-            pattern6(5);
-        }
-        static void pattern6(int n){
-           for (int row = 1; row <=2*n; row++) {
-            int c=row>n ? 2*n-row: row;
+    public static void main(String[] args) {
+        pattern6(5);
+    }
 
-            for (int spaces = 0; spaces < n-c; spaces++) {
+    static void pattern6(int n) {
+        for (int row = 1; row <= 2 * n; row++) {
+            int c = row > n ? 2 * n - row : row;
+
+            for (int spaces = 0; spaces < n - c; spaces++) {
                 System.out.print("  ");
             }
             for (int col = c; col >= 1; col--) {
-                System.out.print(col +"  ");
+                System.out.print(col + "  ");
             }
-            for (int col = 2; col <=c; col++) {
-                System.out.print(col +" ");
+            for (int col = 2; col <= c; col++) {
+                System.out.print(col + " ");
             }
             System.out.println();
-           }
+        }
     }
-} 
-
-
+}
