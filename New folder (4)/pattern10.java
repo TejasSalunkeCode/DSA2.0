cb46882f11e169c1ceaf9f59e2a835f93415190d
TejasSalunkeCode <tejassalunke8788@gmail.com>
2025-02-20 22:18:40 +0530

    file update

--- New folder (4)/pattern10.java
+++ New folder (4)/pattern10.java
@@ -1,8 +1,16 @@
 public class pattern10 {
    public static void main(String[] args) {
-    
+    printpattern(4);
    } 
-   static void print(){
-    
+   static void printpattern(int n){
+      int originalIn=n;
+      n=2*n;
+      for (int row = 0; row <=n; row++) {
+         for (int col = 0; col <=n; col++) {
+            int ateveryIndex=originalIn-Math.min(Math.min(row,col),Math.min(n-row,n-col));
+            System.out.print(ateveryIndex + " ");
+         }
+         System.out.println();
+      }
    }
 }
