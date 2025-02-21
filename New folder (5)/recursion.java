7e639316394a00ce926c01b73572c9c1c186e48d
TejasSalunkeCode <tejassalunke8788@gmail.com>
2025-02-21 21:34:52 +0530

    updated

---
+++ New folder (5)/recursion.java
@@ -0,0 +1,14 @@
+public class recursion {
+    public static void main(String[] args) {
+        print(1);
+    }   
+    static void print(int n){
+        if(n==5){
+            return;
+        }
+        System.out.println(n);
+        print(n+1);
+    } 
+}
+
+
