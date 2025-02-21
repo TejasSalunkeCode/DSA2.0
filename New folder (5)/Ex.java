43dcc741c3bd031396379a8bf9cda118730cf72b
TejasSalunkeCode <tejassalunke8788@gmail.com>
2025-02-21 18:23:15 +0530

    updated

---
+++ New folder (5)/Ex.java
@@ -0,0 +1,20 @@
+public class Ex {
+ public static void main(String[] args) {
+    print();
+ }    
+ static void print(){
+    System.out.println("Hello World");
+    print1();
+ }
+ static void print1(){
+    System.out.println("Hello World");
+    print2();
+ }
+ static void print2(){
+    System.out.println("Hello World");
+    print3();
+ }
+ static void print3(){
+    System.out.println("Hello World");
+ }
+}
