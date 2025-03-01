79009d54aa443e34c40bdab86db88e1567551d7d
TejasSalunkeCode <tejassalunke8788@gmail.com>
2025-03-01 12:16:59 +0530

    file upadte

--- New folder (6)/odd_.java
+++ New folder (6)/odd_.java
@@ -1,7 +1,7 @@
 public class odd_ {
     public static void main(String[] args) {
-        int n=79;
-        System.out.println(oddd(n));
+            int n=79;
+            System.out.println(oddd(n));
     }
     private static boolean oddd(int n){
         return (n & 1) == 1;
