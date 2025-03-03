3717ac4b111d146b6735a583faeb37704f2cda11
TejasSalunkeCode <tejassalunke8788@gmail.com>
2025-03-03 23:45:38 +0530

    update

--- New folder (6)/BinarySearchsqroot.java
+++ New folder (6)/BinarySearchsqroot.java
@@ -1,37 +1,37 @@
 public class BinarySearchsqroot {
     public static void main(String[] args) {
-        int n=40;
-        int p=3;
+        int n = 40;
+        int p = 3;
 
-        System.out.println(sqrt(n,p));
+        System.out.println(sqrt(n, p));
     }
 
-    static double sqrt(int n,int p){
-        int s=0;
-        int e=n;
+    static double sqrt(int n, int p) {
+        int s = 0;
+        int e = n;
 
-        double root=0.0;
+        double root = 0.0;
 
-        while (s<=e) {
-            int m=s+(e-s)/2;
+        while (s <= e) {
+            int m = s + (e - s) / 2;
 
-            if(m*m==n){
+            if (m * m == n) {
                 return m;
             }
 
-            if(m*m > n){
-                e=m-1;
-            }else{
-                s=m+1;
+            if (m * m > n) {
+                e = m - 1;
+            } else {
+                s = m + 1;
             }
         }
-        double incr=0.1;
+        double incr = 0.1;
         for (int i = 0; i < p; i++) {
-            while (root*root<=n) {
-                root+=incr;
+            while (root * root <= n) {
+                root += incr;
             }
-            root-=incr;
-            incr/=10;
+            root -= incr;
+            incr /= 10;
         }
         return root;
     }
