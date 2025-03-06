03fae1f0a030b1bce563636eb1ac59dc4da04f57
TejasSalunkeCode <tejassalunke8788@gmail.com>
2025-03-06 23:41:52 +0530

    chsnge in file

--- New folder (6)/factors.java
+++ New folder (6)/factors.java
@@ -1,13 +1,36 @@
 public class factors {
     public static void main(String[] args) {
         findFact(20);
+        findFact2(20);
     }
     static void findFact(int n){
-        int sum=0;
+
+        int count=1;
         for (int i = 1; i < n; i++) {
             if(n%i==0){
-                System.out.print(i+",");
+                System.out.print(i+" ");
             }
+            count++;
         }
+        System.out.print("loop "+count);
+        System.out.println();
     }
+
+
+    static void findFact2(int n){
+
+        int count=0;
+        for (int i = 1; i <Math.sqrt(n); i++) {
+            if(n%i==0){
+                if(n%i==i){
+                    System.out.print(i+" ");
+                }else{
+                    System.out.print(i+" "+n/i+" ");
+                }
+            }
+            count++;
+        }
+        System.out.println("loop "+count);
+    }
+
 }
