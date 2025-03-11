7f1bd7923ed9b96e0a33684a4b81b4f3fea8efb2
TejasSalunkeCode <tejassalunke8788@gmail.com>
2025-03-11 22:12:04 +0530

    minor change

--- 28_reursion_pattern/selection.java
+++ 28_reursion_pattern/selection.java
@@ -2,28 +2,27 @@ import java.util.Arrays;
 
 public class selection {
     public static void main(String[] args) {
-        int[] arr={9,6,0,4,2,1,5};
+        int[] arr = { 9, 6, 0, 4, 2, 1, 5 };
         selectionn(arr, arr.length, 0, 0);
         System.out.println(Arrays.toString(arr));
     }
-    static void selectionn(int[] arr,int r,int c,int max){
-        if(r==0){
+
+    static void selectionn(int[] arr, int r, int c, int max) {
+        if (r == 0) {
             return;
         }
 
-        
-        if(c<r){
-            if(arr[c]>arr[max]){
-                selectionn(arr, r, c+1, c);
-            }else{
-                selectionn(arr, r, c+1, max);
+        if (c < r) {
+            if (arr[c] > arr[max]) {
+                selectionn(arr, r, c + 1, c);
+            } else {
+                selectionn(arr, r, c + 1, max);
             }
-        }
-        else{
-            int temp=arr[max];
-            arr[max]=arr[r-1];
-            arr[r-1]=temp;
-            selectionn(arr, r-1, 0, 0);
+        } else {
+            int temp = arr[max];
+            arr[max] = arr[r - 1];
+            arr[r - 1] = temp;
+            selectionn(arr, r - 1, 0, 0);
         }
     }
 }
