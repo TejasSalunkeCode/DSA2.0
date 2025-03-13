1e2faedfcaeaf017bbbe7c85e392133319407a9f
TejasSalunkeCode <tejassalunke8788@gmail.com>
2025-03-13 23:05:44 +0530

    minor change

--- New folder (3)/PrettyPrinting.java
+++ New folder (3)/PrettyPrinting.java
@@ -1,11 +1,10 @@
 public class PrettyPrinting {
   public static void main(String[] args) {
-    float f=564.3216f;
-    System.out.printf("Formated number is %.2f      ",f);
+    float f = 564.3216f;
+    System.out.printf("Formated number is %.2f      ", f);
 
+    System.out.printf("Piee : %.3f ", Math.PI);
 
-    System.out.printf("Piee : %.3f ",Math.PI);
-
-    System.out.printf("Hello my name is %s and I am %s ","Tejas","Cool");
-  }  
+    System.out.printf("Hello my name is %s and I am %s ", "Tejas", "Cool");
+  }
 }
