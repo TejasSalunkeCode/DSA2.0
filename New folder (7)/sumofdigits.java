84968b688b8a90ff5eef892831c518e27873313c
TejasSalunkeCode <tejassalunke8788@gmail.com>
2025-03-14 21:18:50 +0530

    minor change

--- New folder (7)/sumofdigits.java
+++ New folder (7)/sumofdigits.java
@@ -1,12 +1,13 @@
 public class sumofdigits {
-public static void main(String[] args) {
-    System.out.println(sumof(123));
-}    
-    static int sumof(int n){
-        if(n==0){
+    public static void main(String[] args) {
+        System.out.println(sumof(123));
+    }
+
+    static int sumof(int n) {
+        if (n == 0) {
             return 0;
         }
         // return sumof(n/10)+(n%10);
-        return 1+(sumof(n));
+        return 1 + (sumof(n));
     }
 }
