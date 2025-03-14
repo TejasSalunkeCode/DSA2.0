c4b6d378a53bb9462c3286a060aaf3be2d674120
TejasSalunkeCode <tejassalunke8788@gmail.com>
2025-03-14 21:17:39 +0530

    update

--- New folder (7)/NoOfStepsZero.java
+++ New folder (7)/NoOfStepsZero.java
@@ -2,16 +2,18 @@ public class NoOfStepsZero {
     public static void main(String[] args) {
         System.out.println(numberofsteps(41));
     }
-    static int numberofsteps(int num){
-        return helper(num,0);
+
+    static int numberofsteps(int num) {
+        return helper(num, 0);
     }
-    static int helper(int num,int steps){
-        if(num==0){
+
+    static int helper(int num, int steps) {
+        if (num == 0) {
             return steps;
         }
-        if(num%2==0){
-            return helper(num/2, steps+1);
+        if (num % 2 == 0) {
+            return helper(num / 2, steps + 1);
         }
-        return helper(num-1, steps+1);
+        return helper(num - 1, steps + 1);
     }
 }
