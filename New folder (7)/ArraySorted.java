7978225bc16c7170dcc77630c60cb24fadbd99b5
TejasSalunkeCode <tejassalunke8788@gmail.com>
2025-03-17 21:18:31 +0530

    update

--- New folder (7)/ArraySorted.java
+++ New folder (7)/ArraySorted.java
@@ -3,6 +3,7 @@ public class ArraySorted {
         int[]arr={3,5,7,9,11,44,56};
         System.out.println(issorted(arr,0));
     }   
+    
     static boolean issorted(int[]arr,int index){
         if(index==arr.length-1){
             return true;
