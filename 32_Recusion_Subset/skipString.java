e16397c6204c87dcd6385d45912fdb346be9accf
TejasSalunkeCode <tejassalunke8788@gmail.com>
2025-05-20 14:08:26 +0530

    subsetString vid-32

--- 32_Recusion_Subset/skipString.java
+++ 32_Recusion_Subset/skipString.java
@@ -1,9 +1,10 @@
 
 public class skipString {
     public static void main(String[] args) {
-        String word="HelloappleTejas";
+        String word="HelloappeTejas";
 
-        System.out.println(skip(word));
+        // System.out.println(skip(word));
+        System.out.println(skipApp(word));
     }
     static String skip(String up){
         if(up.isEmpty()){
@@ -15,6 +16,18 @@ public class skipString {
             return skip(up.substring(5));
         }else{
             return up.charAt(0)+ skip(up.substring(1));
+        }
+    }
+     static String skipApp(String up){
+        if(up.isEmpty()){
+            // System.out.println(p);
+            return "";
+        }
+        // char ch=up.charAt(0);
+        if(up.startsWith("app") && ! up.startsWith("apple")){
+            return skipApp(up.substring(3));
+        }else{
+            return up.charAt(0)+ skipApp(up.substring(1));
         }
     }
 }
