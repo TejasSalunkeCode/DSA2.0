64be3a77307aac2a303ab18048b5feeb26134021
TejasSalunkeCode <tejassalunke8788@gmail.com>
2025-05-21 12:34:35 +0530

    ascill valuee

--- 32_Recusion_Subset/subSetString.java
+++ 32_Recusion_Subset/subSetString.java
@@ -1,6 +1,10 @@
+import java.util.ArrayList;
+
 public class subSetString {
     public static void main(String[] args) {
-        subseq("", "abc");
+        // subseq("", "abc");
+        System.out.println(subseqRet("","abc"));
+ 
     }
     static void subseq(String p,String up){
         if(up.isEmpty()){
@@ -11,5 +15,21 @@ public class subSetString {
 
         subseq(p+ch, up.substring(1));
         subseq(p, up.substring(1));
+    }
+
+    static ArrayList<String> subseqRet(String p,String up){
+        if(up.isEmpty()){
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            // System.out.println(p);
+            return list;
+        }
+        char ch=up.charAt(0);
+
+        ArrayList<String> left=subseqRet(p+ch, up.substring(1));
+        ArrayList<String> right=subseqRet(p, up.substring(1));
+
+        left.addAll(right);
+        return left;
     }
 }
