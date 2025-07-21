720abf7d72c826efc6635318de0b5d2d72574c0f
TejasSalunkeCode <tejassalunke8788@gmail.com>
2025-07-21 23:49:36 +0530

    try to solve peoblem

--- search.java
+++ search.java
@@ -3,39 +3,73 @@ import java.util.Arrays;
 
 public class search {
     public static void main(String[] args) {
-        char[][] arr = {
-                { 'A', 'B', 'C', 'E' },
-                { 'S', 'F', 'C', 'S' },
-                { 'A', 'D', 'E', 'E' }
-        };
-        String word = "ABCCED";
-        System.out.println(find(arr, word));
-
+        int[] height = { 1, 8, 6, 2, 5, 4, 8, 3, 7 };
+        System.out.println(maxArea(height));
     }
 
-    static boolean find(char[][] board, String word) {
+    static int maxArea(int[] height) {
+        int start = 0;
+        int end = height.length - 1;
+        int width;
+        int ans = 0;
+        int Finalans = 0;
+        int Finalans2 = 0;
+        int Finalans3 = 0;
+        int Finalans4 = 0;
+        while (start < end) {
+            width = end - start;
+            ans = height[start] * width;
+            int minHeight;
+            if (height[start] < height[end]) {
+                start++;
+                width--;
+                minHeight = Math.min(height[start], height[end]);
+                System.out.println("Minimum " + minHeight);
+                System.out.println("width " + width);
+                // Finalans = 0;
 
-        
-        String ans = "";
-        int length=word.length();
-        System.out.println(board.length);
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[i].length; j++) {
-                System.out.print(board[i][j]);
+                // System.out.println(start);
+                // return start;
+                Finalans = minHeight * width;
+                System.out.println("final ans in 1st Block : " + Finalans);
             }
-            System.out.println();
-        }
-        System.out.println("word: "+ans);
-        int i=0;
-        System.out.println("next");
-        while (length!=0) {
-            for (int j = 0; j < board.length; j++) {
-                for (int j2 = 0; j2 < board[i].length; j2++) {
-                    
-                }
+            if (height[start] > height[end]) {
+                // Finalans = height[start] * width;
+                // System.out.println("j");
+                end--;
+                width--;
+                minHeight = Math.min(height[start], height[end]);
+                // Finalans = 0;
+                // System.out.println(start);
+                // return start;
+                Finalans2 = minHeight * width;
+                System.out.println("final ans in 2nd Block : " + Finalans2);
             }
+            if (height[start] == height[end]) {
+                // Finalans = height[start] * width;
+                // System.out.println("j");
+                start++;
+                width--;
+                minHeight = Math.min(height[start], height[end]);
+                // Finalans = 0;
+                // System.out.println(start);
+                // return start;
+                Finalans3 = minHeight * width;
+                System.out.println("final ans in 3rd Block : " + Finalans3);
+            }
+            // System.out.println(ans);
         }
-        
-        return true;
+        if (ans > Finalans && ans > Finalans2 && ans > Finalans3) {
+            return ans;
+        } else if (Finalans > ans && Finalans > Finalans2 && Finalans > Finalans3) {
+                return Finalans;
+            
+        }else if (Finalans2 > ans && Finalans2 > Finalans && Finalans2 > Finalans3) {
+                return Finalans2;
+            
+        }else{
+            return Finalans3;
+        }
+        // return -1;
     }
 }
