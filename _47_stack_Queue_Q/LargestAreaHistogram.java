6fe09ca78db8034924888706f57e24dcbe01810c
TejasSalunkeCode <tejassalunke8788@gmail.com>
2025-08-02 15:01:53 +0530

    LargestAreaHistogram [vid-47]

---
+++ _47_stack_Queue_Q/LargestAreaHistogram.java
@@ -0,0 +1,40 @@
+package _47_stack_Queue_Q;
+
+import java.util.Stack;
+
+public class LargestAreaHistogram {
+    public static void main(String[] args) {
+        int []heights={2,1,5,6,1,3};
+        System.out.println(LargestAreaHistogram(heights));
+    }   
+    static int LargestAreaHistogram(int[]heights){
+        Stack<Integer> stack = new Stack<>();
+        int max=0;
+
+        stack.push(0);
+
+        for (int i = 1; i < heights.length; i++) {
+            while (!stack.isEmpty() && heights[i] < heights[stack.peek()]) {
+                max=getMax(heights, stack, max, i);
+            }
+            stack.push(i);
+        }
+
+        int i=heights.length;
+        while (!stack.isEmpty()) {
+            max=getMax(heights, stack, max, i);
+        }
+        return max;
+    } 
+
+    private static int getMax(int[]arr,Stack<Integer> stack,int max,int i){
+        int area;
+        int popped=stack.pop();
+        if(stack.isEmpty()){
+            area=arr[popped]*i;
+        }else{
+            area=arr[popped]*(i-1-stack.peek());
+        }
+        return Math.max(max, area);
+    }
+}
