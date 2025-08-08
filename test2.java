15e978d4c2fe466300995429e583c0635832bd30
TejasSalunkeCode <tejassalunke8788@gmail.com>
2025-08-08 22:47:24 +0530

    solve leetcode 136 number problem

--- test2.java
+++ test2.java
@@ -1,43 +1,20 @@
-import java.util.Arrays;
 
-import Interface.breake;
 
 public class test2 {
 
-    public static void main(String[] args) {
-        int[] fruits = { 0,0,1,1,1,2,2,3,3,4};
-        // int[] baskets = { 6, 4, 7 };
-        System.out.println(removeDuplicates(fruits));
-    }
-
-    static int removeDuplicates(int[] nums) {
-        int count = 0; 
-
-        int[] nums2 = new int[nums.length];
-        int k = 0;
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                if (nums[i] == nums[j]) {
-                    if(nums2[k]!=nums[i]){
-                        nums2[k] = nums[i];
-                    }
-                    count++;
-                    k++;
-                }
-                // else{
-                //     nums2[k]=nums[j];
-                // }
-                // if (nums[i] == nums[j]) {
-                //     nums2[k] = nums[j];
-                //     k++;
-                // }
-                // break
-            }
+    public int singleNumber(int[] nums) {
+        int result = 0;
+        for (int num : nums) {
+            // Manual XOR using: (a OR b) AND NOT (a AND b)
+            result = (result | num) & ~(result & num);
         }
-
-        System.out.println(Arrays.toString(nums2));
-        System.out.println(nums.length - count);
-        return nums.length - count;
+        return result;
     }
 
+    // You can test it here
+    public static void main(String[] args) {
+        test2 sol = new test2();
+        int[] nums = {4, 1, 2, 1, 2};
+        System.out.println(sol.singleNumber(nums)); // Output: 4
+    }
 }
