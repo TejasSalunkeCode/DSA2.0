361e3598d5be64279bba36e6fc5b0e163fc96c4d
TejasSalunkeCode <tejassalunke8788@gmail.com>
2025-08-15 20:09:15 +0530

    tree [vid-49]

--- _49_Tree/BinaryTree.java
+++ _49_Tree/BinaryTree.java
@@ -61,6 +61,44 @@ public class BinaryTree {
         display(node.right, indent + "\t");
     }
 
+    public void preOrder() {
+        preOrder(root);
+    }
+
+    private void preOrder(Node node) {
+        if (node == null) {
+            return;
+        }
+        System.out.println(node.value + " ");
+        preOrder(node.left);
+        preOrder(node.right);
+    }
+
+    public void InOrder() {
+        InOrder(root);
+    }
+
+    private void InOrder(Node node) {
+        if (node == null) {
+            return;
+        }
+        InOrder(node.left);
+        System.out.println(node.value + " ");
+        InOrder(node.right);
+    }
+
+        public void postOrder(){
+        postOrder(root);
+    }
+    private void postOrder(Node node){
+        if(node==null){
+            return;
+        }
+        postOrder(node.left);
+        postOrder(node.right);
+        System.out.println(node.value+" ");
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         BinaryTree tree = new BinaryTree();
