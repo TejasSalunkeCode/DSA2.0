87fb787357873c2c51e5a366dae1b0f585c800d5
TejasSalunkeCode <tejassalunke8788@gmail.com>
2025-08-17 21:14:32 +0530

    leetcode 321 number problem solved

--- 2QU/leetcode_1323.java
+++ 2QU/leetcode_1323.java
@@ -1,4 +1,3 @@
-import java.lang.reflect.Array;
 import java.util.Arrays;
 
 public class leetcode_1323 {
---
+++ 2QU/leetcode_387.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+
+public class leetcode_387 {
+
+    public static void main(String[] args) {
+        String s = "loveleetcode";
+        System.out.println(firstUniqChar(s));
+    }
+
+    static int firstUniqChar(String s) {
+        char[] arr = s.toCharArray();
+        System.out.println(Arrays.toString(arr));
+        int count=0;
+        System.out.println(s.length());
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                if (arr[i] != arr[j]) {
+                    count++;
+                    // continue;
+                }
+            }
+            if(count==s.length()-1){
+                return i;
+            }
+            System.out.println(count);
+            count=0;
+        }
+
+        return -1;
+    }
