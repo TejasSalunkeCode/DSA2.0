68e03c548c13ec69c8d665095a79c4e26875c5d0
TejasSalunkeCode <tejassalunke8788@gmail.com>
2025-09-02 16:47:51 +0530

    leetcode 98 solved

--- _55_Binary_Tree_Ques/DFS/dfss.java
+++ _55_Binary_Tree_Ques/DFS/dfss.java
@@ -93,4 +93,25 @@ public class dfss {
         }
     }
 
+
+    // Q.98
+    public boolean isValidBST(TreeNode root) {
+    return  helper(root,null,null);
+    }
+    public boolean helper(TreeNode node,Integer low,Integer high){
+        if(node==null){
+            return true;
+        }
+        if(low!=null && node.val<=low){
+            return false;
+        }
+        if(high!=null && node.val>=high){
+            return false;
+        }
+        boolean left=helper(node.left, low, node.val);
+        boolean right=helper(node.right, node.val, high);
+
+        return left && right;
+    }
+
 }
