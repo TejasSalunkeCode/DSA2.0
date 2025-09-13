d82d8f473203496c79e13cc6d10da606a9187bc8
TejasSalunkeCode <tejassalunke8788@gmail.com>
2025-09-13 13:47:24 +0530

    solved daily Q

---
+++ Daily/leetcode_3541.java
@@ -0,0 +1,33 @@
+public class leetcode_3541 {
+    public static void main(String[] args) {
+        String s = "successes";
+        System.out.println(maxFreqSum(s));  // Expected: 6
+    }
+
+    static int maxFreqSum(String s) {
+        int[] freq = new int[26]; 
+        for (char c : s.toCharArray()) {
+            freq[c - 'a']++;
+        }
+
+        int maxVowel = 0;
+        int maxConsonant = 0;
+
+        for (int i = 0; i < 26; i++) {
+            if (freq[i] > 0) {
+                char c = (char) (i + 'a');
+                if (isVowel(c)) {
+                    maxVowel = Math.max(maxVowel, freq[i]);
+                } else {
+                    maxConsonant = Math.max(maxConsonant, freq[i]);
+                }
+            }
+        }
+
+        return maxVowel + maxConsonant;
+    }
+
+    static boolean isVowel(char c) {
+        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
+    }
+}
