4888e345f7e9515ff487fda442b7b00ed65243ec
TejasSalunkeCode <tejassalunke8788@gmail.com>
2026-02-13 00:18:56 +0530

    leetcode 443

--- 2QU/leetcode_443.java
+++ 2QU/leetcode_443.java
@@ -1,6 +1,6 @@
 public class leetcode_443 {
     public static void main(String[] args) {
-        char[] chars = { 'a', 'a', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b' };
+        char[] chars = {'a','b','c','d' };
         System.out.println(compress(chars));
     }
 
@@ -10,16 +10,38 @@ public class leetcode_443 {
         int count = 0;
         int index = 0;
         int k = 0;
-
-        for (int i = 0; i < chars.length; i++) {
+        int j = 0;
+        if(chars.length==1){
+            return 1;
+        }
+        for (int i = 0; i < chars.length+1; i++) {
             currentchar = chars[k];
-            if (currentchar == chars[i]) {
+            if (i < chars.length && currentchar == chars[i]) {
                 count++;
+                // if(i+1>chars.length){
+                // break;
+                // }
             } else {
+                System.out.println("hello"); 
                 k = i;
-                if (count != 1) {
+                if(count==1){
+                    chars[index]=currentchar;
+                    index++;
+                }
+                else{
                     chars[index] = currentchar;
                     String cnt = Integer.toString(count);
+                    int len = cnt.length();
+                    while (len != 0) {
+                        if (len > 0) {
+                            index++;
+                            chars[index] = cnt.charAt(j);
+                            j++;
+                            len--;
+                        }
+                    }
+                    j=0;
+                    index++;
                 }
                 System.out.println(count);
                 count = 1;
