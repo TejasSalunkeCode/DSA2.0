a590116c1819e24f2e0d35354e1661baa59b6b7d
TejasSalunkeCode <tejassalunke8788@gmail.com>
2026-03-12 17:48:34 +0530

    add launch configuration for leetcode problem 3 and update solution logic

--- 2QU/leetcode_3.java
+++ 2QU/leetcode_3.java
@@ -1,9 +1,34 @@
+import java.util.HashMap;
+
 public class leetcode_3 {
     public static void main(String[] args) {
-        String s = "abcabcbb";
+        String s = "jbpnbwwd";
         System.out.println(lengthOfLongestSubstring(s));
     }
      public static int lengthOfLongestSubstring(String s) {
-        
+        if(s.length()==0){
+            return 0;
+        }
+        HashMap<Character,Integer> map = new HashMap<>();
+        int cnt=0;
+        int val=0;
+        int max=Integer.MIN_VALUE;
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = i; j < s.length(); j++) {
+                if(map.containsKey(s.charAt(j))){
+                    cnt=0;
+                    val=0;
+                    map.clear();
+                }else{
+                    cnt++;
+                    map.put(s.charAt(j), val+1);
+                }
+                System.out.println(map);
+                max=Math.max(max, cnt);
+            }
+            map.clear();
+            cnt=0;
+        }
+        return max;
     }
 }
