acdffdcebddac29a3a1ec4ff5ddc861ca1fb466b
TejasSalunkeCode <tejassalunke8788@gmail.com>
2026-03-17 21:40:47 +0530

    add launch configuration for leetcode problem 1727 and implement solution

---
+++ 2QU/leetcode_1727.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+
+public class leetcode_1727 {
+    public static void main(String[] args) {
+        int[][] grid = {
+            {0, 0, 1},
+            {1, 1, 1},
+            {1, 0, 1}
+        };
+
+        int result = largestSubmatrix(grid);
+        System.out.println("Largest Submatrix Area: " + result);
+    }
+    public static int largestSubmatrix(int[][]grid){
+        int m=grid.length,n=grid[0].length;
+        int ans=0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if(i>0 && grid[i][j]>0){
+                    grid[i][j]+=grid[i-1][j];
+                }
+            }
+            int temp[]=grid[i].clone();
+            Arrays.sort(temp);
+            for (int k = 0; k < n; k++) {
+                int width=n-k;
+                ans=Math.max(ans, temp[k]*width);
+            }
+        }
+        return ans;
+    }
+}
