693f4bd4d3aa7c1db23a2642642747e73245e9b5
TejasSalunkeCode <tejassalunke8788@gmail.com>
2026-03-20 09:16:22 +0530

    style: add a blank line for better readability in leetcode_2.java

--- 2QU/leetcode_2.java
+++ 2QU/leetcode_2.java
@@ -45,6 +45,7 @@ public class leetcode_2 {
         ListNode tail = dummyHead;
         int carry = 0;
 
+        
         while (l1 != null || l2 != null || carry != 0) {
             int digit1 = (l1 != null) ? l1.val : 0;
             int digit2 = (l2 != null) ? l2.val : 0;
