9c1a2c9572950f257dd081bb5da5ab67a72aba7a
TejasSalunkeCode <tejassalunke8788@gmail.com>
2026-03-27 21:54:51 +0530

    feat: add new Java class leetcode_2946 with similarity check logic

---
+++ 2QU/leetcode_2946.java
@@ -0,0 +1,21 @@
+public class leetcode_2946 {
+    public static void main(String[] args) {
+        int[][]mat = {{1,2,3},{4,5,6},{7,8,9}}; 
+        int k = 4;
+
+        System.out.println(areSimilar(mat, k));
+    }
+
+    public static boolean areSimilar(int[][] mat, int k) {
+        int m=mat[0].length;
+        k%=m;
+        for (int i = 0; i < mat.length; i++) {
+            for (int j = 0; j < mat[0].length; j++) {
+                if(mat[i][j]!=mat[i][(j+k)%m]){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
